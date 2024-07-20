b4502e656f33bad5cd3f357eaf1c48b5622b1974
JaimeDev0 <Jaime01dev@hotmail.com>
2024-07-20 18:54:03 +0200

    Introduccion y Variables

---
+++ Basic/00-Introduccion.js
@@ -0,0 +1,42 @@
+
+
+
+//Es un lenguaje dinamico es que puedes cambiar los tipos de las variables en tiempo de ejecucion
+//it is a dynamic language where you can change the types of variables at runtime
+
+let text = 'Hola mundo'
+console.log(text)
+text = 2
+console.log(text)
+
+// En un lenguaje de tipado débil, no necesitas asignar un tipo fijo a la variable.
+// it is devil tip language, dont need assignet a type fixed of the variable        | Mio
+// In a weakly typed language, you don't need to assing a fixed type to a variable
+
+
+//Tipos de datos JavaScript hay 7
+//tips of dates JavaScript here seven
+
+// primitives
+console.log(typeof undefined)   // undefined
+console.log(typeof true)        // boolean
+console.log(typeof 2)           // number
+console.log(typeof "string")    // string
+console.log(typeof 2n)          //bigInt
+console.log(typeof Symbol())    //symbol
+
+console.log(typeof null)        //null es especial por un error de js deberia dar null pero da object
+
+
+//Objetos   objects
+typeof {}
+typeof []
+const set = new Map()
+typeof set
+
+//Funcion   fuction
+typeof function () {}
+
+
+
+
---
+++ Basic/01-variables.js
@@ -0,0 +1,24 @@
+// guardar valor o referancia en memoria
+
+
+//var   NO ES RECOMENDABLE USARLO
+var helloWorld = "¡Hello, JavaScript!"
+console.log(helloWorld)
+
+helloWorld = "¡Hola de nuevo, JavaScript!"
+console.log(helloWorld)
+
+//let Solo accesible dentro de un bloque
+let helloWorld2 = "¡Hello, JavaScript 2!"
+console.log(helloWorld2)
+
+helloWorld2 = "¡Hola de nuevo, JavaScript 2!"
+console.log(helloWorld2)
+
+//const
+const helloWorld3 = "¡Hello, JavaScript 3!"
+console.log(helloWorld3)
+
+//Error pero si se puede tener referencias
+//helloWorld3 = "No se puede reasignar"
+console.log(helloWorld3)
