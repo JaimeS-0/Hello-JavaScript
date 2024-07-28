938b99169052cc473b2affd99fced9f40ce9912a
JaimeDev0 <Jaime01dev@hotmail.com>
2024-07-28 20:36:09 +0200

    add contend to Strings-exersises

--- Basic/07-strings-exercises.js
+++ Basic/07-strings-exercises.js
@@ -7,20 +7,90 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 
 // 1. Concatena dos cadenas de texto
 
+console.log("-------------------------------------------")
+console.log("1. Concatena dos cadenas de texto")
+console.log("-------------------------------------------")
+
+    const name = `Aitana`
+    const secondName = " Ocaña Morales"
+    console.log(name + secondName)
+
+    console.log('Hola tengo oro y quiero '
+    + 'Un cohete.' )
+
+       
 // 2. Muestra la longitud de una cadena de texto
+
+console.log("-------------------------------------------")
+console.log("2. Muestra la longitud de una cadena de texto")
+console.log("-------------------------------------------")
+
+    const description = `Hola Buenos dias`
+
+    console.log("La cadena de texto tiene " + description.length)
 
 // 3. Muestra el primer y último carácter de un string
 
+console.log("-------------------------------------------")
+console.log("3. Muestra el primer y último carácter de un string")
+console.log("-------------------------------------------")
+
+    const planet = "Planet central"
+    console.log(planet.length + " | " + planet)
+    console.log("Muestra el primer caracter: " + planet[0])
+    console.log("Muestra el ultimo caracter: " + planet[13])
+
 // 4. Convierte a mayúsculas y minúsculas un string
+
+console.log("-------------------------------------------")
+console.log("4. Convierte a mayúsculas y minúsculas un string")
+console.log("-------------------------------------------")
+
+    const word = "parque de atracciones" 
+    console.log(word.toUpperCase())
+
+    const word1 = "CABAÑA EN EL BOSQUE"
+    console.log(word1.toLowerCase())
 
 // 5. Crea una cadena de texto en varias líneas
 
+console.log("-------------------------------------------")
+console.log(" 5. Crea una cadena de texto en varias líneas")
+console.log("-------------------------------------------")
+
+    const text = 'Hola me quiero ' 
+    + 'comprar una estacion'
+    console.log(text)
+
 // 6. Interpola el valor de una variable en un string
+
+console.log("-------------------------------------------")
+console.log("6. Interpola el valor de una variable en un string")
+console.log("-------------------------------------------")
+
+    let goddess  = "Taylor Alison Swift"
+    console.log(`Quien es una diosa en la industia musical?  ${goddess}`)
 
 // 7. Reemplaza todos los espacios en blanco de un string por guiones
 
+console.log("-------------------------------------------")
+console.log("7. Reemplaza todos los espacios en blanco de un string por guiones")
+console.log("-------------------------------------------")
+
 // 8. Comprueba si una cadena de texto contiene una palabra concreta
+
+console.log("-------------------------------------------")
+console.log("8. Comprueba si una cadena de texto contiene una palabra concreta")
+console.log("-------------------------------------------")
 
 // 9. Comprueba si dos strings son iguales
 
+console.log("-------------------------------------------")
+console.log("9. Comprueba si dos strings son iguales")
+console.log("-------------------------------------------")
+
 // 10. Comprueba si dos strings tienen la misma longitud
+
+console.log("-------------------------------------------")
+console.log("10. Comprueba si dos strings tienen la misma longitud")
+console.log("-------------------------------------------")
