fbc3a41fd2a6916c5f4226590b3ec911a2fdeff1
JaimeDev0 <Jaime01dev@hotmail.com>
2024-07-31 17:18:34 +0200

    add new exercises

--- Basic/08-conditionals.js
+++ Basic/08-conditionals.js
@@ -25,3 +25,41 @@ if (age == 37) {
 } else {
     console.log("La edad no es 37")
 }
+
+// Operador Ternario
+
+const message = age == 22 ? "La edad es 22" : "La edad no es 22"
+console.log(message)
+
+// switch
+
+let day = 3
+let dayName
+
+switch (day) {
+    case 0:
+        dayName = "Lunes"
+        break
+    case 1:
+        dayName = "Martes" 
+        break
+    case 2:
+        dayName = "Miercoles" 
+        break
+    case 3:
+        dayName = "Jueves" 
+        break
+    case 4:
+        dayName = "Viernes" 
+        break
+    case 5:
+        dayName = "Sabado" 
+        break
+    case 6:
+        dayName = "Domingo"  
+        break
+    default: 
+        dayName == "Número de día incorrecto"   
+}
+
+console.log(dayName)
