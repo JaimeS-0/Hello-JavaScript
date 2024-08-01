804015027759549bc58c2a032f060c654522e4e4
Jaime <jaime01dev@hotmail.com>
2024-08-01 09:13:34 +0200

    add exercises

--- Basic/09-condisionals-exercises.js
+++ Basic/09-condisionals-exercises.js
@@ -8,7 +8,23 @@ https://www.twitch.tv/videos/2206228701?t=00h16m02s
 
 // 1. Imprime por consola tu nombre si una variable toma su valor
 
+    let Name = "Jaime"
+    console.log(Name)
+
 // 2. Imprime por consola un mensaje si el usuario y contraseña concide con unos establecidos
+
+    let user = 'Paco'
+    let password = 123456
+    
+    if (user == 'Paco') {
+        console.log("El usuario coincide con Nombre: " + user)    
+    } else if(user != 'Paco') {
+        console.log("El usuario no es igual al Nombre: " + user)
+    } else if (password == 123456) {
+        console.log("La contraseña coincide con el valor establecido.")
+    } else (password != 123456) 
+        console.log("La contraseña no es igual")
+    
 
 // 3. Verifica si un número es positivo, negativo o cero e imprime un mensaje
 
