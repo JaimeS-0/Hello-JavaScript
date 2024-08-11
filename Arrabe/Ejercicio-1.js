eb96795515fb984e1720ce1d39e31b87be343ed4
JaimeDev0 <Jaime01dev@hotmail.com>
2024-08-11 18:40:25 +0200

    add new change

---
+++ Arrabe/Ejercicio-1.js
@@ -0,0 +1,12 @@
+console.log("Estados del servidor")
+
+const estado_100 = "Continuar"
+const estado_103 = "Primeros Avisos"
+const estado_202 = "Aceptado"
+const estado_204 = "Sin Contenido"
+const estado_206 = "Contenido Parcial"
+const estado_301 = "Movido Permanentemente"
+const estado_303 = "Ver Otro"
+const estado_400 = "Solicitud Incorrecta"
+const estado_404 = "No Encontrado"
+const estado_500 = "Error Interno del Servidor"
---
+++ Basic/10-array.js
@@ -0,0 +1,15 @@
+// array | Estructura de datos
+
+// Declaracion
+
+let myArray = []
+let myArray2 = new Array()
+
+console.log(myArray)
+console.log(myArray2)
+
+myArray = [3]
+myArray2 = new Array(3)
+
+console.log(myArray)
+console.log(myArray2)
